73254ffacb38becf81a0b1a6e09fddde6c35125a
luis salazar <escor.g39@gmail.com>
2025-05-12 13:04:20 -0400

    de su reputisima madre socio

--- store/models.py
+++ store/models.py
@@ -1,4 +1,21 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    products = models.ManyToManyField(Product)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Orden #{self.id}"
 
